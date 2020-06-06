40239cedd18a41e0adc26e262d4a75a81090f280
Nikita <Nikita@TheVer4.ru>
2020-06-06 14:46:26 +0500

    Initial commit

---
+++ AjaxDemo/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AjaxDemo.Models;
+
+namespace AjaxDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Chess()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+
+        [HttpGet]
+        public bool GetInfo(string type, int x1, int y1, int x2, int y2)
+        { //https://localhost:5001/Home/GetInfo?type=horse&x1=1&y1=2&x2=3&y2=4
+            return type.ToLower() switch
+            {
+                "pawn" => ChessLogic.Pawn(x1, y1, x2, y2),
+                "bishop" => ChessLogic.Bishop(x1, y1, x2, y2),
+                "king" => ChessLogic.King(x1, y1, x2, y2),
+                "queen" => ChessLogic.Queen(x1, y1, x2, y2),
+                "knight" => ChessLogic.Knight(x1, y1, x2, y2),
+                "rook" => ChessLogic.Rook(x1, y1, x2, y2),
+                _ => false
+            };
+        }
+    }
+}
---
+++ AjaxDemo/Models/ChessLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AjaxDemo.Models
+{
+    public class ChessLogic
+    {
+        public static bool Pawn(int x1, int y1, int x2, int y2) //Пешка
+        {
+            return x2 == x1 && (Math.Abs(y2 - y1) == 2 || Math.Abs(y2 - y1) == 1);
+        }
+        
+        public static bool Bishop(int x1, int y1, int x2, int y2) //Слон
+        {
+            return Math.Abs(x2 - x1) == Math.Abs(y2 - y1) && x2 != x1;
+        }
+        
+        public static bool King(int x1, int y1, int x2, int y2) //Король
+        {
+            return (Math.Abs(x2 - x1) <= 1 || Math.Abs(y2 - y1) <= 1) && Math.Abs(x2 - x1) + Math.Abs(y2 - y1) != 0;
+        }
+        
+        public static bool Queen(int x1, int y1, int x2, int y2) //Королева
+        {
+            return Rook(x1, y1, x2, y2) || Bishop(x1, y1, x2, y2);
+        }
+        
+        public static bool Knight(int x1, int y1, int x2, int y2) //Конь
+        {
+            return Math.Abs(x2 - x1) + Math.Abs(y2 - y1) == 3 && x2 != x1 && y2 != y1;
+        }
+        
+        public static bool Rook(int x1, int y1, int x2, int y2) //Ладья
+        {
+            return x2 == x1 ^ y2 == y1;
+        }
+    }
+}
